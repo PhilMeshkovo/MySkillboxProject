7e74027eaf99c90019c2df8829ca7422832f684f
Blinov Philipp <meshkovo1977@mail.ru>
2020-09-07 09:49:05 +0300

    clearDb plug

--- src/main/resources/db.changelog/testdata/test_data.sql
+++ src/main/resources/db.changelog/testdata/test_data.sql
@@ -2,15 +2,15 @@
 BEGIN;
 
 INSERT INTO users(id, is_moderator, reg_time, name, email, password, code, photo) VALUES
-(1, -1,  '2020-01-06','Bonita Boyer',  'tarakan@mail.ru', '$2y$12$tfeBz8zV72agV/JS/UJy4OHD7sEcPsyworySRx0jaTgpQljHRe4Jm', '1234', '/api/image/default-1.png'),
-(2, 1,'2020-09-30', 'Philipp',  'meshkovo1977@mail.ru', '$2y$12$tfeBz8zV72agV/JS/UJy4OHD7sEcPsyworySRx0jaTgpQljHRe4Jm', '1234', '1234');
+(1, -1,'2020-01-06','Bonita Boyer', 'tarakan@mail.ru', '$2y$12$tfeBz8zV72agV/JS/UJy4OHD7sEcPsyworySRx0jaTgpQljHRe4Jm', '1234', '/api/image/default-1.png'),
+(2, 1,'2020-09-30', 'Philipp', 'meshkovo1977@mail.ru', '$2y$12$tfeBz8zV72agV/JS/UJy4OHD7sEcPsyworySRx0jaTgpQljHRe4Jm', '1234', '1234');
 
 INSERT INTO posts(id, moderation_status,moderator_id, view_count, time, user_id, title, text, is_active) VALUES
 (1,'ACCEPTED',2,7, '2020-01-01 00:28:11', 1, 'Non nostrum dignissimos.', 'Focusing on the latest investigations, we can positively say that the example of the feedback system must stay true to an importance of the well-known practice.  ',1),
 (2,'ACCEPTED',1,3, '2020-01-01 00:28:11', 2, 'Java and spring boot', '2e the framework of the primary element gives less satisfactory results.', 1 );
 
 INSERT INTO post_comments(id, time, post_id, parent_id, user_id, text) VALUES
-(1, '2020-01-01 00:00:07', 1, null, 2, 'By the way, with help of the skills leads us to a clear understanding of the conceptual design.  '),
+(1, '2020-01-01 00:00:07', 1, null, 2, 'By the way, with help of the skills leads us to a clear understanding of the conceptual design. '),
 (2, '2020-07-23 05:4:15', 2, null, 1, 'In a word, each of the task analysis impacts typically on every market tendencies.');
 --(3, '2006-12-23 23:20:32', 9, 2, 8, 'What is more, the condition of the treatment provides a strict control over the sufficient amount.'),
 --(4, '1974-05-31 10:02:51', 6, null, 7, 'By the way, any further consideration cannot be developed under such circumstances.'),
@@ -38,7 +38,7 @@ INSERT INTO tags(id, name) VALUES
 (2, 'kotlin'),
 (3, 'php'),
 (4, 'python'),
-(5, 'tagName'),
+(5, 'junit'),
 (6, 'spring'),
 (7, 'sql'),
 (8, 'postgres'),
